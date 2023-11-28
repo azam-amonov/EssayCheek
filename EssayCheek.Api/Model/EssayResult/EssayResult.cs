8bdaf1233d8b60d020f9db572cf0a50d67a16cfc
Azam Amonov <azamamonov555@gmail.com>
2023-11-28 12:57:15 +0500

    N/A

---
+++ EssayCheek.Api/Model/EssayResult/EssayResult.cs
@@ -0,0 +1,19 @@
+using System.Text.Json.Serialization;
+using EssayCheek.Api.Model.Essays;
+
+namespace EssayCheek.Api.Model.EssayResult;
+
+public class EssayResult
+{
+    public Guid Id { get; set; }
+
+    public string Feedback { get; set; }
+    
+    public int Score { get; set; }
+    
+    [JsonIgnore]
+    public Guid EssayId { get; set; }
+
+    [JsonIgnore] 
+    public virtual Essay Essay {get; set; }
+}
