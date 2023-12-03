f49122d0ed3cd043107965c12cbd36c17a07ae63
Azam Amonov <azamamonov555@gmail.com>
2023-12-04 00:23:57 +0500

    ShouldTheCriticalDependencyExceptionOnAddIfSqlErrorOccursLogItAsync -> PASS

---
+++ EssayCheek.Api/Model/Foundation/Users/Exceptions/UserDependencyException.cs
@@ -0,0 +1,12 @@
+using Xeptions;
+
+namespace EssayCheek.Api.Model.Foundation.Users.Exceptions;
+
+public class UserDependencyException : Xeption
+{
+    public UserDependencyException(Exception innerException) 
+                    : base("User dependency error occured, contact support.", innerException)
+    {
+        
+    }
+}
