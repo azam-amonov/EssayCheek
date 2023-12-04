23ac56f553769fd36d29430f0d92844b01cd7fee
Azam Amonov <azamamonov555@gmail.com>
2023-12-04 23:16:43 +0500

    ShouldThrowFoundExceptionOnRetrieveByIdUserIsNotFoundAndLogItAsync -> FAIL

--- EssayCheek.API.UnitTest/Services/Foundations/Users/UserServiceTest.Logic.RetrieveById.cs
+++ EssayCheek.API.UnitTest/Services/Foundations/Users/UserServiceTest.Logic.RetrieveById.cs
@@ -1,4 +1,5 @@
 using EssayCheek.Api.Model.Foundation.Users;
+using EssayCheek.Api.Model.Foundation.Users.Exceptions;
 using FluentAssertions;
 using Force.DeepCloner;
 using Moq;
@@ -34,6 +35,47 @@ public partial class UserServiceTest
         _storageBrokerMock.Verify(broker =>
             broker.SelectUserByIdAsync(inputUserId)
                 ,Times.Once);
+        
+        _storageBrokerMock.VerifyNoOtherCalls();
+        _loggingBrokerMock.VerifyNoOtherCalls();
+        _dateTimeBrokerMock.VerifyNoOtherCalls();
+    }
+
+    [Fact]
+    public async Task ShouldThrowFoundExceptionOnRetrieveByIdUserIsNotFoundAndLogItAsync()
+    {
+        // given
+        Guid someUserId = Guid.NewGuid();
+        User? noUser = null;
+
+        var notFountUserException = new NotFoundUserException(someUserId);
+
+        var expectedUserValidationException = 
+                new UserValidationException(notFountUserException);
+
+        _storageBrokerMock.Setup(broker => 
+                broker.SelectUserByIdAsync(It.IsAny<Guid>()))
+                        .ReturnsAsync(noUser);
+
+        // when
+        ValueTask<User> retrieveUserByIdTask =
+                _userService.RetrieveUserByIdAsync(someUserId);
+
+        UserValidationException actualUserValidationException =
+                await Assert.ThrowsAsync<UserValidationException>(
+                                retrieveUserByIdTask.AsTask);
+        
+        // then
+        actualUserValidationException.Should().BeEquivalentTo(expectedUserValidationException);
+        
+        _storageBrokerMock.Verify(broker => 
+                    broker.SelectUserByIdAsync(It.IsAny<Guid>()),
+                        Times.Once());
+        
+        _loggingBrokerMock.Verify(broker =>
+                        broker.LogError(It.Is(SameExceptionAs(
+                                        expectedUserValidationException))),
+                        Times.Once);
         
         _storageBrokerMock.VerifyNoOtherCalls();
         _loggingBrokerMock.VerifyNoOtherCalls();
---
+++ EssayCheek.Api/Model/Foundation/Users/Exceptions/NotFoundUserException.cs
@@ -0,0 +1,12 @@
+using Xeptions;
+
+namespace EssayCheek.Api.Model.Foundation.Users.Exceptions;
+
+public class NotFoundUserException : Xeption
+{
+    public NotFoundUserException(Guid userId) 
+        : base($"Couldn't find user with id {userId}.")
+    {
+        
+    }
+}
