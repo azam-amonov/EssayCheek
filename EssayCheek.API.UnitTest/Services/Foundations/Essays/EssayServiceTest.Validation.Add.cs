876d7cd9f6c3cc36ac03c33c9b67a512e03c155a
Azam Amonov <azamamonov555@gmail.com>
2023-12-08 20:00:40 +0500

    ShouldValidationExceptionOnAddIfEssayIsNullAsync -> PASS

---
+++ EssayCheek.API.UnitTest/Services/Foundations/Essays/EssayServiceTest.Validation.Add.cs
@@ -0,0 +1,92 @@
+using EssayCheek.Api.Model.Foundation.Essays;
+using EssayCheek.Api.Model.Foundation.Essays.Exceptions;
+using FluentAssertions;
+using Moq;
+
+namespace EssayCheek.API.UnitTest.Services.Foundations.Essays;
+
+public partial class EssayServiceTest
+{
+    [Fact]
+    public async Task ShouldValidationExceptionOnAddIfEssayIsNullAsync()
+    {
+       //given
+       Essay? nullEssay = null;
+       var nullEssayException = new EssayNullException();
+
+       var expectedEssayValidationException = new EssayValidationException(nullEssayException);
+
+       //when
+       ValueTask<Essay> addEssayTask = _essayService.AddEssayAsync(nullEssay);
+
+       EssayValidationException actualEssayValidationException =
+                       await Assert.ThrowsAsync<EssayValidationException>(addEssayTask.AsTask);
+       
+       //then
+       actualEssayValidationException.Should().BeEquivalentTo(expectedEssayValidationException);
+
+       _loggingBrokerMock.Verify(broker => 
+                       broker.LogError(It.Is(SameExceptionAs(
+                            expectedEssayValidationException))),
+                       Times.Once);
+       
+       _loggingBrokerMock.VerifyNoOtherCalls();
+       _storageBrokerMock.VerifyNoOtherCalls();
+    }
+    
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("  ")]
+    public async Task ShouldThrowValidationExceptionOnAddIfEssayIsInvalidAndLogItAsync(string invalidText)
+    {
+       //given
+       var invalidEssay = new Essay()
+       {
+                       Title = invalidText,
+                       Content = invalidText
+       };
+    
+       var expectedInvalidEssayException = new InvalidEssayException();
+       
+       expectedInvalidEssayException.AddData(
+                       key: nameof(Essay.Id),
+                       values:"Id is required");
+       
+       expectedInvalidEssayException.AddData(
+                       key: nameof(Essay.Title),
+                       values:"Text is required");
+       
+       expectedInvalidEssayException.AddData(
+                       key: nameof(Essay.Content),
+                       values:"Text is required");
+       
+       expectedInvalidEssayException.AddData(
+                       key: nameof(Essay.SubmittedDate),
+                       values:"Date is required");
+    
+       var expectedEssayValidationException =
+                       new EssayValidationException(expectedInvalidEssayException);
+       
+       //when
+       ValueTask<Essay> addEssayTask = _essayService.AddEssayAsync(invalidEssay);
+    
+       EssayValidationException actualEssayValidationException =
+                       await Assert.ThrowsAsync<EssayValidationException>(addEssayTask.AsTask);
+       
+       //then
+       actualEssayValidationException.Should().BeEquivalentTo(expectedEssayValidationException);
+       
+       _loggingBrokerMock.Verify(broker => 
+                       broker.LogError(It.Is(SameExceptionAs(
+                                       expectedEssayValidationException))),Times.Once);
+       
+       _storageBrokerMock.Verify(broker =>
+                       broker.InsertEssayAsync(invalidEssay), Times.Never);
+       
+       _loggingBrokerMock.VerifyNoOtherCalls();
+       _storageBrokerMock.VerifyNoOtherCalls();
+       _dateTimeBrokerMock.VerifyNoOtherCalls();
+    }
+    
+}
