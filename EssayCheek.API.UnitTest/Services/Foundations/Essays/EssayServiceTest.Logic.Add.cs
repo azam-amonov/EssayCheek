e03db58699b302f6f23e2246380fda7107e69490
Azam Amonov <azamamonov555@gmail.com>
2023-12-08 20:01:49 +0500

    CODE RUB: cleanup

--- EssayCheek.API.UnitTest/Services/Foundations/Essays/EssayServiceTest.Logic.Add.cs
+++ EssayCheek.API.UnitTest/Services/Foundations/Essays/EssayServiceTest.Logic.Add.cs
@@ -33,63 +33,7 @@ public partial class EssayServiceTest
         _loggingBrokerMock.VerifyNoOtherCalls();
     }
 
-    [Theory]
-    [InlineData(null)]
-    [InlineData("")]
-    [InlineData("  ")]
-    public async Task ShouldThrowValidationExceptionOnAddIfEssayIsInvalidAndLogItAsync(string invalidText)
-    {
-       //given
-       var invalidEssay = new Essay()
-       {
-                       Title = invalidText,
-                       Content = invalidText
-       };
-
-       var expectedInvalidEssayException = new InvalidEssayException();
-       
-       expectedInvalidEssayException.AddData(
-                       key: nameof(Essay.Id),
-                       values:"Id is required");
-       
-       expectedInvalidEssayException.AddData(
-                       key: nameof(Essay.UserId),
-                       values: "Id is required");
-       
-       expectedInvalidEssayException.AddData(
-                       key: nameof(Essay.Title),
-                       values:"Text is required");
-       
-       expectedInvalidEssayException.AddData(
-                       key: nameof(Essay.Content),
-                       values:"Text is required");
-       
-       expectedInvalidEssayException.AddData(
-                       key: nameof(Essay.SubmittedDate),
-                       values:"Date is required");
-
-       var expectedEssayValidationException =
-                       new EssayValidationException(expectedInvalidEssayException);
-       
-       //when
-       ValueTask<Essay> addEssayTask = _essayService.AddEssayAsync(invalidEssay);
-
-       EssayValidationException actualEssayValidationException =
-                       await Assert.ThrowsAsync<EssayValidationException>(addEssayTask.AsTask);
-       
-       //then
-       actualEssayValidationException.Should().BeEquivalentTo(expectedEssayValidationException);
-       
-       _loggingBrokerMock.Verify(broker => 
-                       broker.LogError(It.Is(SameExceptionAs(
-                                       expectedEssayValidationException))),Times.Once);
-       
-       _storageBrokerMock.Verify(broker =>
-                       broker.InsertEssayAsync(invalidEssay), Times.Never);
-       
-       _storageBrokerMock.VerifyNoOtherCalls();
-       _loggingBrokerMock.VerifyNoOtherCalls();
-    }
+    
 }
 
 
